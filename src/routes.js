b062973aa0bb51f487e5c2b9f9e38391102ede03
kukajp3 <kukajp@gmail.com>
2020-04-15 22:48:59 -0300

    add appointment delete

--- src/routes.js
+++ src/routes.js
@@ -28,9 +28,11 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedules', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 export default routes;
